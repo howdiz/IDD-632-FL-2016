6a23aa8475bfb26992df2425cccd30fbbfeb5be4
Howie <howie@o3world.com>
2016-09-18 19:09:05 -0400

    create brewery form

---
+++ beerApp/breweryForm.php
@@ -0,0 +1,23 @@
+<h1>Enter Brewery</h1>
+<form action="addBrewery.php" method="post">
+    <div>
+        <label for="name">Name:</label>
+        <input type="text" name="name" />
+    </div>
+    <div>
+        <label for="city">city:</label>
+        <input type="text" name="city" />
+    </div>
+    <div>
+        <label for="state">state:</label>
+        <input type="text" name="state" />
+    </div>
+    </div>
+    <div>
+        <label for="country">country:</label>
+        <input type="text" name="country" />
+    </div>
+    <div class="button">
+        <button type="submit">submit</button>
+    </div>
+</form>
