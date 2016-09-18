1fed88b9903e1a09c564945a1d1033063518a819
Howie <howie@o3world.com>
2016-09-18 19:49:20 -0400

    add beer query output

--- beerApp/index.php
+++ beerApp/index.php
@@ -12,9 +12,26 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+$sql = "SELECT
+  Beers.name as beerName, style, abv, rating, notes,
+  Breweries.name as BreweryName, city, state, country
+FROM Beers JOIN Breweries ON Breweries.id = Beers.brewery_id";
+$result = $conn->query($sql);
+
 ?>
 
 <h1>Beers</h1>
 
 <h2><a href="breweryForm.php">Add a Brewery</a></h2>
 <h2><a href="beerForm.php">Add a Beer</a></h2>
+
+<?php
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo $row['beerName'] . " | " . $row['style'] . " | " . $row['abv'] .
+        " | " . $row['BreweryName'] . " | " . $row['city'] . " | " . $row['state'] .
+         " | " . $row['country'] ."<br>";
+    }
+}
+?>
