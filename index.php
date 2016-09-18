bd0c25abbeeb84a877e1363ce30a361eb4e2ed2c
Howie <howie@o3world.com>
2016-09-18 18:44:33 -0400

    add basic contact form to index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<form action="thanks.php" method="post">
+    <div>
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" />
+    </div>
+    <div>
+        <label for="mail">E-mail:</label>
+        <input type="email" id="mail" name="email" />
+    </div>
+    <div>
+        <label for="msg">Message:</label>
+        <textarea id="message" name="message"></textarea>
+    </div>
+
+    <div class="button">
+        <button type="submit">Send your message</button>
+    </div>
+</form>
