87e26ad979b16a3181c413451e621aae996219b1
Howie <howie@o3world.com>
2016-09-28 15:20:43 -0400

    add nav include

---
+++ beerApp/nav.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="/class/beerApp">BeerApp</a>
+    </div>
+    <div id="navbar" class="collapse navbar-collapse">
+      <ul class="nav navbar-nav">
+        <li><a href="breweryForm.php">Add Brewery</a></li>
+        <li><a href="beerForm.php">Add Beer</a></li>
+      </ul>
+    </div><!--/.nav-collapse -->
+  </div>
+</nav>
